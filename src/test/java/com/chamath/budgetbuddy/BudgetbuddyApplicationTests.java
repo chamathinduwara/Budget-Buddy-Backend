b81074a8b80b2491c91f2955d05e5934b22832ee
chamath <csschamathinduwara@gmail.com>
2023-11-18 13:20:03 +0530

    initial commit

---
+++ src/test/java/com/chamath/budgetbuddy/BudgetbuddyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chamath.budgetbuddy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BudgetbuddyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
